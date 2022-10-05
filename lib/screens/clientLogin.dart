eba42f4c8402742d7e52bcde3c0b8108657a6645
ayushpatel2004 <ayushpatel140604@gmail.com>
2022-10-05 22:10:57 +0530

    I merged Log In button to homePage.

---
+++ lib/screens/clientLogin.dart
@@ -0,0 +1,127 @@
+// import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import '/utils/constants.dart';
+import '../utils/button.dart';
+import '/screens/clientRegistration.dart';
+import 'homePage.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: kPrimaryColor,
+      ),
+      home: Scaffold(
+        resizeToAvoidBottomInset : false,
+        backgroundColor: kBackgroundColor,
+        appBar: AppBar(
+          title: const Text(
+            'Bhawan Apps',
+          ),
+          centerTitle: true,
+          backgroundColor: kPrimaryColor,
+        ),
+        body: SingleChildScrollView(
+          physics: NeverScrollableScrollPhysics(),
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minWidth: MediaQuery.of(context).size.width,
+              minHeight: MediaQuery.of(context).size.height,
+            ),
+            child: IntrinsicHeight(
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        child: Container(
+                          // width: 200,
+                          // height: ,
+                          child: Image(
+                            image: AssetImage('images/logo.jpg'),
+                          ),
+                        ),
+                      ),
+                      // Container(
+                      //     child: Text(
+                      //       'Bhawan App',
+                      //       style: TextStyle(
+                      //         fontSize: 30,
+                      //         fontWeight: FontWeight.bold,
+                      //         color: kPrimaryColor,
+                      //       ),
+                      //     ),
+                      // ),
+                    ],
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(15.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        labelText: 'Username',
+                        hintText: 'Enter your username here',
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(15.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        labelText: 'Password',
+                        hintText: 'Enter your Password here',
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(15.0),
+                          child: Container(
+                            width: 150,
+                            child: Button(
+                              'Sign Up',
+                              main1,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(15.0),
+                          child: Container(
+                            width: 150,
+                            child: Button(
+                              'Log In',
+                              main3,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+        ),
+      ),
+    );
+  }
+}
