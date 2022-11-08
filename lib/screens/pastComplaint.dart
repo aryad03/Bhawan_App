7ad3faed2f2c18ea5f95b40bfca5d59975b83c63
Arya Deshmukh <aryaad2003@gmail.com>
2022-11-08 20:19:29 +0530

    inserted loading screen

--- lib/screens/pastComplaint.dart
+++ lib/screens/pastComplaint.dart
@@ -1,3 +1,4 @@
+import 'package:bhawan_app/utils/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -36,7 +37,8 @@ class PastComplaints extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final usercomplain = Provider.of<List<UserComplain>>(context);
+    final usercomplain = Provider.of<List<UserComplain>?>(context);
+    if(usercomplain!=null){
     return MaterialApp(
       home: Scaffold(
         body: Column(
@@ -143,6 +145,9 @@ class PastComplaints extends StatelessWidget {
           ],
         ),
       ),
-    );
+    );}
+    else{
+      return Loading();
+    }
   }
 }
